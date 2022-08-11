08c6af737fb721aa791667de3df5a1d275dfe196
Cardoso <lucas95felipe@gmail.com>
2022-08-11 14:33:57 -0300

    ex002 presencial

---
+++ ex002.js
@@ -0,0 +1,18 @@
+let lista = 14
+
+for(let numero = 0 ;numero <=14 ; numero ++){
+
+    if ( numero == 0 ) {
+        console.log(numero + " numero é zero e par")
+    }
+
+    else if(numero % 2 === 0){   
+        console.log(numero + " numero é par")
+    
+    }
+
+    else{
+    console.log (numero + " numero é impar")
+    }
+
+}
